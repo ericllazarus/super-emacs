7e1edf82384d773a1559c8abb31db58ff8208dca
Eric Lazarus <LazarusJavaScript@gmail.com>
2019-06-19 17:44:38 -0400

    Add md.txt

--- .emacs.d/super-emacs/06-vars-etc.el
+++ .emacs.d/super-emacs/06-vars-etc.el
@@ -1,3 +1,15 @@
+(require 'package)
 
 (setq ispell-program-name "/usr/local/bin/ispell")
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+(package-initialize)
+
+
+(add-to-list 'auto-mode-alist '("\\.md.txt\\'" . markdown-mode))
+
+(dolist (hook '(text-mode-hook))
+       (add-hook hook (lambda () (flyspell-mode 1))))
 
