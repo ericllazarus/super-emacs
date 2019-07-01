fd5a839885db1e45604e0b71135c1b30ab0ead2a
Eric Lazarus <LazarusJavaScript@gmail.com>
2019-07-01 02:41:21 -0400

    Add markdown mode for .md.txt files and comment out M-/

--- .emacs.d/super-emacs/06-vars-etc.el
+++ .emacs.d/super-emacs/06-vars-etc.el
@@ -2,11 +2,10 @@
 
 (setq ispell-program-name "/usr/local/bin/ispell")
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;(add-to-list 'package-archives
+;             '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-(package-initialize)
-
+;(package-initialize)
 
 (add-to-list 'auto-mode-alist '("\\.md.txt\\'" . markdown-mode))
 
